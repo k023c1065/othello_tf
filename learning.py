b094076510854b5796350db0c48a34d936bc86f9
k023c1065 <k023c1065@g.neec.ac.jp>
2023-10-05 09:18:18 +0900

    minor fix

--- learning.py
+++ learning.py
@@ -17,14 +17,14 @@ def main(EPOCH=10,batch_size=32):
     print(pd.DataFrame(pd.Series(x.ravel()).describe()).transpose())
     print(pd.DataFrame(pd.Series(np.array(y,dtype="float32").reshape(y.shape[0],64).ravel()).describe()).transpose())
     print("-------Describe End------")
-    x_train,x_test,y_train,y_test=train_test_split(x,np.array(y,dtype="float32").reshape(y.shape[0],64),test_size=0.2,random_state=0)
+    x_train,x_test,y_train,y_test=train_test_split(x,np.array(y,dtype="float32").reshape(y.shape[0],64),test_size=0.25,random_state=0)
     #=train_test_split(,random_state=0)
     test_ds = tf.data.Dataset.from_tensor_slices(
                 (x_test,y_test)
             ).batch(64)
     train_ds = tf.data.Dataset.from_tensor_slices(
             (x_train, y_train)).shuffle(10000).batch(batch_size)
-    model=ResNet((2,8,8),64)
+    model=ResNet((2,64,64),64)
     optimizer=tf.optimizers.Adam()
     loss_object=tf.keras.losses.categorical_crossentropy
     t_module=train_module(model,loss_object,optimizer)
