2b86d4f68c218319a49681c4610324650676669d
k023c1065 <k023c1065@g.neec.ac.jp>
2023-10-07 17:55:31 +0900

    minor change

--- learning.py
+++ learning.py
@@ -22,6 +22,8 @@ def main(EPOCH=10,batch_size=16,input_shape=(224,224,2)):
     print("-------Describe End------")
     x_train,x_test,y_train,y_test=train_test_split(x,np.array(y,dtype="float32").reshape(y.shape[0],64),test_size=0.25,random_state=0)
     #=train_test_split(,random_state=0)
+    print(x_train.shape)
+    print(y_train.shape)
     test_ds = tf.data.Dataset.from_tensor_slices(
                 (x_test,y_test)
             ).batch(64)
