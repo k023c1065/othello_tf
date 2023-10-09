96e4a4d97f2081a47be4d9f51ca4f1447be5a4ad
k023c1065 <k023c1065@g.neec.ac.jp>
2023-10-09 14:08:28 +0900

    minor fix

--- create_dataset.py
+++ create_dataset.py
@@ -67,7 +67,7 @@ def sub_create_dataset(play_num,expand_rate,p_num,Lock):
                 a+=max(0,(1-score[i]/sum(score)))
                 a[d[1][0]][d[1][1]]=score[i]/sum(score)
                 dataset[1].append(a.reshape(64))
-        dataset[1]=tf.nn.softmax(dataset[1],axis=1)
+    dataset[1]=np.array(tf.nn.softmax(dataset[1],axis=1))
     return dataset
 if __name__=="__main__":
     main(proc_num=4,play_num=200,)
