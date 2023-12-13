2a04955e237bfdf928044687220586ec6b0c4977
k023c1065 <k023c1065@g.neec.ac.jp>
2023-12-13 18:00:27 +0900

    minor fix

--- file_log/mylog.py
+++ file_log/mylog.py
@@ -1,6 +1,8 @@
 import logging
 import datetime,os
 from multiprocessing import Lock
+if not os.path.dirname(__file__) == "":
+    os.chdir(os.path.dirname(__file__))
 filename=f"./log/{str(datetime.datetime.now())}.log".replace(" ","").replace(":","_")
 logging.basicConfig(filename=filename,level=logging.INFO,
                     format='%(asctime)s %(message)s')
@@ -8,9 +10,8 @@ log_level=[]
 lock=Lock()
 
 class mylog:
-    print(os.path.dirname(__file__))
-    if not os.path.dirname(__file__) == "":
-        os.chdir(os.path.dirname(__file__))
+
+
     @classmethod
     def add_log(cls,msg,level=0):
         with lock:
